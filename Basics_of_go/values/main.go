25907388c65a10cb43d27c8f8395e98d0b33ba12
Shreehari-Acharya <shreehari.acharya.06@gmail.com>
2026-01-04 20:53:18 +0530

    Basics of go - part 1 : hello, values, variables

---
+++ Basics_of_go/values/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(1 + 1)
+	fmt.Println("a string")
+	fmt.Println(true)
+	fmt.Println(10.5)
+	fmt.Println(10.6 / 14.8)
+}
---
+++ Basics_of_go/variables/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main(){
+	// syntax: var <variable_name> <data_type> = <value>
+	var name string = "a string"
+	// short hand declaration
+	var name2 = "another string"
+
+	// another short hand declaration
+	age := 25
+
+	// printing the variables
+	fmt.Println(age)
+	fmt.Println(name2)
+	fmt.Println(name)
+}
